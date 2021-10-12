26d0dc4e3fb321fea916f0a14c0e5b3bed4b29d5
Zach Earl <zachearl@GOV-ZE-MACBOOKPRO13.local>
2021-10-12 11:45:43 -0400

    troubleshooting

--- notes/settings.py
+++ notes/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
 SOCIAL_AUTH_POSTGRES_JSONFIELD = True
 
 AUTHENTICATION_BACKENDS = (
-    'social_core.backends.open_id.OpenIdAuth',
+    'social_core.backends.OpenIdConnectAuth',
     "social_core.backends.google.GoogleOAuth2",
     "django.contrib.auth.backends.ModelBackend",
 )
